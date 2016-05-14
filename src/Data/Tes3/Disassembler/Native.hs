ed727b1b7e7aeecd2fbdbdd163712308bd7d55be
A1 <internalmike@gmail.com>
2016-05-14 13:45:16 +0300

    Data.Binary.Get

--- src/Data/Tes3/Disassembler/Native.hs
+++ src/Data/Tes3/Disassembler/Native.hs
@@ -1,8 +1,12 @@
 module Data.Tes3.Disassembler.Native where
 
 #include <haskell>
+import Data.Tes3
 
 data T3Error = UnexpectedEOF
+
+sign :: Get T3Sign
+sign = t3SignNew <$> getWord32le
 
 disassembly :: ByteString -> Either T3Error String
 disassembly _ = Right "ESP\n"
--- src/Data/Tes3/Native.hs
+++ src/Data/Tes3/Native.hs
@@ -5,25 +5,25 @@ module Data.Tes3.Native where
 data T3Mark
   = TES3 | FNAM | NAME | SCPT | CONT
   | MISC | NPC_
-  deriving (Eq, Ord, Enum, Bounded, Show, Read)
+  deriving (Eq, Ord, Enum, Bounded, Show)
 
 data T3Sign = T3Mark T3Mark | T3Sign Word32 deriving Eq
 
 instance Show T3Sign where
   show (T3Mark m) = show m
   show (T3Sign s) =
-    let (a, b, c, d) = getBytes s in
+    let (a, b, c, d) = toBytes s in
     [chr $ fromIntegral a, chr $ fromIntegral b, chr $ fromIntegral c, chr $ fromIntegral d]
 
-combineBytes :: Word8 -> Word8 -> Word8 -> Word8 -> Word32
-combineBytes a b c d
+fromBytes :: Word8 -> Word8 -> Word8 -> Word8 -> Word32
+fromBytes a b c d
   =  (fromIntegral a)
   .|. (shift (fromIntegral b) 8)
   .|. (shift (fromIntegral c) 16)
   .|. (shift (fromIntegral d) 24)
   
-getBytes :: Word32 -> (Word8, Word8, Word8, Word8)
-getBytes w =
+toBytes :: Word32 -> (Word8, Word8, Word8, Word8)
+toBytes w =
   ( fromIntegral $ w .&. 0xFF
   , fromIntegral $ (.&. 0xFF) $ shift w (-8)
   , fromIntegral $ (.&. 0xFF) $ shift w (-16)
@@ -34,14 +34,14 @@ t3MarkValueSlow :: T3Mark -> Word32
 t3MarkValueSlow mark =
   calc (show mark)
   where
-    calc [a, b, c, d] = combineBytes (fromIntegral $ ord a) (fromIntegral $ ord b) (fromIntegral $ ord c) (fromIntegral $ ord d)
-    calc _ = error "T3Mark"
+    calc [a, b, c, d] = fromBytes (fromIntegral $ ord a) (fromIntegral $ ord b) (fromIntegral $ ord c) (fromIntegral $ ord d)
+    calc _ = 0x0BAD0BAD
 
 t3MarkValues :: S.Map T3Mark Word32
 t3MarkValues = SM.fromList [(m, t3MarkValueSlow m) | m <- [minBound .. maxBound]]
 
 t3MarkValue :: T3Mark -> Word32
-t3MarkValue m = fromMaybe 0 $ SM.lookup m t3MarkValues
+t3MarkValue m = fromMaybe 0x0BAD0BAD $ SM.lookup m t3MarkValues
 
 t3SignValue :: T3Sign -> Word32
 t3SignValue (T3Mark m) = t3MarkValue m
