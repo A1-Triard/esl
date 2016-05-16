5c090e63957da9a03dc8f150a2d65aeb85eceb74
A1 <internalmike@gmail.com>
2016-05-17 02:09:09 +0300

    Assembler module

---
+++ src/Data/Tes3/Assembler.hs
@@ -0,0 +1,5 @@
+module Data.Tes3.Assembler
+  ( parseT3File
+  ) where
+
+import Data.Tes3.Assembler.Native
---
+++ src/Data/Tes3/Assembler/Native.hs
@@ -0,0 +1,7 @@
+module Data.Tes3.Assembler.Native where
+
+#include <haskell>
+import Data.Tes3
+
+parseT3File :: T.Parser T3File
+parseT3File = error ""
