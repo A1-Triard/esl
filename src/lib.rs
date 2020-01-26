7ba7c247fa3bd195acced33e735f5306105fe2e7
Warlock <internalmike@gmail.com>
2020-01-26 14:23:22 +0300

    Dialog

--- src/lib.rs
+++ src/lib.rs
@@ -18,14 +18,26 @@ macro_attr! {
     #[derive(Ord, PartialOrd, Eq, PartialEq, Hash, Copy, Clone)]
     #[derive(Debug, EnumDisplay!, EnumFromStr!)]
     #[repr(u32)]
-    pub enum FileType {
+    pub enum Tes3 {
         ESP = 0,
         ESM = 1,
         ESS = 32
     }
 }
 
-pub use FileType::*;
+macro_attr! {
+    #[derive(Primitive)]
+    #[derive(Ord, PartialOrd, Eq, PartialEq, Hash, Copy, Clone)]
+    #[derive(Debug, EnumDisplay!, EnumFromStr!)]
+    #[repr(u32)]
+    pub enum Dialog {
+        Topic = 0,
+        Voice = 1,
+        Greeting = 2,
+        Persuasion = 3,
+        Journal = 4
+    }
+}
 
 #[cfg(test)]
 mod tests {
@@ -40,10 +52,10 @@ mod tests {
 
     #[test]
     fn test_file_type() {
-        assert_eq!("ESM", format!("{}", ESM));
-        assert_eq!("ESS", format!("{:?}", ESS));
-        assert_eq!(Some(ESP), FileType::from_u32(0));
-        assert_eq!(None, FileType::from_u32(2));
-        assert_eq!(32, ESS as u32);
+        assert_eq!("ESM", format!("{}", Tes3::ESM));
+        assert_eq!("ESS", format!("{:?}", Tes3::ESS));
+        assert_eq!(Some(Tes3::ESP), Tes3::from_u32(0));
+        assert_eq!(None, Tes3::from_u32(2));
+        assert_eq!(32, Tes3::ESS as u32);
     }
 }
